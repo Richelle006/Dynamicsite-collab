94b627c78defe25b9d8dfb013d354214e916aca7
Richelle006 <12202377@students.koi.edu.au>
2024-05-05 18:08:58 +1000

    Update profile.php

--- profile.php
+++ profile.php
@@ -23,6 +23,12 @@ $sql = "SELECT * FROM Bookings WHERE user_id = ?";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param('i', $user_id);
 $stmt->execute();
+
+// Check for SQL errors
+if ($stmt->errno) {
+    die("SQL Error: " . $stmt->error);
+}
+
 $result = $stmt->get_result();
 
 // Close statement
@@ -42,49 +48,52 @@ $conn->close();
 </head>
 <body>
 <header>
-        <div class="header-content">
-            <img src="resources/logo.png" alt="Framed Memories Studio Logo" class="site-logo">
-        </div>
-        <nav id="navbar">
-            <ul>
-                <li><a href="index.php"><img src="resources/home-button.png" alt="" class="menu-icon">Home</a></li>
-                <li><a href="about.php"><img src="resources/about us.png" alt="" class="menu-icon">About Us</a></li>
-                <li class="dropdown">
-                    <a href="services.php"><img src="resources/services.png" alt="" class="menu-icon">Services</a>
-                    <div class="dropdown-content">
-                        <a href="photo_studio.php">Photo Studio</a>
-                        <a href="customized_frame.php">Customized Frame</a>
-                        <a href="events_workshops.php">Events & Workshops</a>
-                    </div>
-                </li>
-                <li><a href="booking.php"><img src="resources/booking.png" alt="" class="menu-icon">Booking</a></li>
-                <li><a href="contact.php"><img src="resources/contact us.png" alt="" class="menu-icon">Contact</a></li>
-                </ul>
-        </nav>
-    </header>
-    <div class="profile-page">
-        <h2>User Profile</h2>
-        <table>
-            <tr>
-                <th>Booking ID</th>
-                <th>Booking Date</th>
-                <th>Service</th>
-                <th>Description</th>
-                <th>Action</th>
-            </tr>
-            <?php while ($row = $result->fetch_assoc()): ?>
-            <tr>
-                <td><?php echo $row['booking_id']; ?></td>
-                <td><?php echo $row['booking_date']; ?></td>
-                <td><?php echo $row['service_id']; ?></td>
-                <td><?php echo $row['description']; ?></td>
-                <td>
-                    <a href="edit_booking.php?id=<?php echo $row['booking_id']; ?>">Edit</a>
-                    <a href="delete_booking.php?id=<?php echo $row['booking_id']; ?>">Delete</a>
-                </td>
-            </tr>
-            <?php endwhile; ?>
-        </table>
+    <div class="header-content">
+        <img src="resources/logo.png" alt="Framed Memories Studio Logo" class="site-logo">
     </div>
+    <nav id="navbar">
+        <ul>
+            <li><a href="index.php"><img src="resources/home-button.png" alt="" class="menu-icon">Home</a></li>
+            <li><a href="about.php"><img src="resources/about us.png" alt="" class="menu-icon">About Us</a></li>
+            <li class="dropdown">
+                <a href="services.php"><img src="resources/services.png" alt="" class="menu-icon">Services</a>
+                <div class="dropdown-content">
+                    <a href="photo_studio.php">Photo Studio</a>
+                    <a href="customized_frame.php">Customized Frame</a>
+                    <a href="events_workshops.php">Events & Workshops</a>
+                </div>
+            </li>
+            <li><a href="booking.php"><img src="resources/booking.png" alt="" class="menu-icon">Booking</a></li>
+            <li><a href="contact.php"><img src="resources/contact us.png" alt="" class="menu-icon">Contact</a></li>
+            <li><a href="index.php">Logout</a></li> 
+        </ul>
+    </nav>
+</header>
+
+<div class="profile-page">
+    <h2>User Profile</h2>
+    <table>
+        <tr>
+            <th>Booking ID</th>
+            <th>Booking Date</th>
+            <th>Service</th>
+            <th>Description</th>
+            <th>Action</th>
+        </tr>
+        <?php while ($row = $result->fetch_assoc()): ?>
+        <tr>
+            <td><?php echo $row['booking_id']; ?></td>
+            <td><?php echo $row['booking_date']; ?></td>
+            <td><?php echo $row['service_id']; ?></td>
+            <td><?php echo $row['description']; ?></td>
+            <td>
+                <a href="edit_booking.php?id=<?php echo $row['booking_id']; ?>">Edit</a>
+                <a href="delete_booking.php?id=<?php echo $row['booking_id']; ?>">Delete</a>
+            </td>
+        </tr>
+        <?php endwhile; ?>
+    </table>
+</div>
+
 </body>
 </html>
