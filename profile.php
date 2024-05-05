6f63eba6400acaadc406f761124a48d6d8df867d
Richelle006 <12202377@students.koi.edu.au>
2024-05-05 17:55:27 +1000

    Update profile.php

--- profile.php
+++ profile.php
@@ -41,6 +41,27 @@ $conn->close();
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
+<header>
+        <div class="header-content">
+            <img src="resources/logo.png" alt="Framed Memories Studio Logo" class="site-logo">
+        </div>
+        <nav id="navbar">
+            <ul>
+                <li><a href="index.php"><img src="resources/home-button.png" alt="" class="menu-icon">Home</a></li>
+                <li><a href="about.php"><img src="resources/about us.png" alt="" class="menu-icon">About Us</a></li>
+                <li class="dropdown">
+                    <a href="services.php"><img src="resources/services.png" alt="" class="menu-icon">Services</a>
+                    <div class="dropdown-content">
+                        <a href="photo_studio.php">Photo Studio</a>
+                        <a href="customized_frame.php">Customized Frame</a>
+                        <a href="events_workshops.php">Events & Workshops</a>
+                    </div>
+                </li>
+                <li><a href="booking.php"><img src="resources/booking.png" alt="" class="menu-icon">Booking</a></li>
+                <li><a href="contact.php"><img src="resources/contact us.png" alt="" class="menu-icon">Contact</a></li>
+                </ul>
+        </nav>
+    </header>
     <div class="profile-page">
         <h2>User Profile</h2>
         <table>
