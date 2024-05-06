70e3e9c0449511f1843be435eca4e6c99eb39910
Richelle006 <12202377@students.koi.edu.au>
2024-05-06 18:12:19 +1000

    Update profile.php

--- profile.php
+++ profile.php
@@ -48,29 +48,6 @@ $conn->close();
 </head>
 <body>
 <header>
-    <div class="profile-page">
-        <h2>User Profile</h2>
-        <table>
-            <tr>
-                <th>Booking ID</th>
-                <th>Booking Date</th>
-                <th>Service</th>
-                <th>Description</th>
-                <th>Action</th>
-            </tr>
-            <?php while ($row = $result->fetch_assoc()): ?>
-            <tr>
-                <td><?php echo $row['booking_id']; ?></td>
-                <td><?php echo $row['booking_date']; ?></td>
-                <td><?php echo $row['service_id']; ?></td>
-                <td><?php echo $row['description']; ?></td>
-                <td>
-                    <a href="edit_booking.php?id=<?php echo $row['booking_id']; ?>">Edit</a>
-                    <a href="delete_booking.php?id=<?php echo $row['booking_id']; ?>">Delete</a>
-                </td>
-            </tr>
-            <?php endwhile; ?>
-        </table>
     <div class="header-content">
         <img src="resources/logo.png" alt="Framed Memories Studio Logo" class="site-logo">
     </div>
